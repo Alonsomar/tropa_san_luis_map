10af6180d6d40c8c9498a105399167942986dea3
Alonso Valdes <alonsoo8.5@gmail.com>
2025-02-19 21:19:45 +0100

    added favicon

--- src/components/CampInfo.svelte
+++ src/components/CampInfo.svelte
@@ -51,6 +51,8 @@
     box-shadow: 0 8px 32px rgba(0, 0, 0, 0.1);
     margin-bottom: 0.5rem;
     backdrop-filter: blur(3px);
+    -webkit-backdrop-filter: blur(3px);
+    border: 1px solid rgba(255, 255, 255, 0.05);
   }
 
   h2 {
